8597fcf1d5007e6f7cd6be2aef81f4f8b7154ce8
janossik <marcinczaniecki97@gmail.com>
2023-07-16 01:12:57 +0200

    added send for Response

--- src/Response.ts
+++ src/Response.ts
@@ -5,6 +5,11 @@ export class Response<TRequest extends IncomingMessage = IncomingMessage> extend
     this.statusCode = code;
     return this;
   }
+  send(value: string) {
+    this.write(value);
+    this.end();
+    return this;
+  }
   json(value: object) {
     this.write(JSON.stringify(value));
     this.end();
