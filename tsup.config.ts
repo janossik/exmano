f67830e27946e10e484a4e02de61981fca2e6bbc
janossik <marcinczaniecki97@gmail.com>
2023-07-16 01:17:51 +0200

    edited config tsup, now tsup will be to compile index.ts and all files ts from src directory

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: [
-    'index.ts',
-    'src/Server.ts',
-    'src/Application.ts',
-    'src/Request.ts',
-    'src/Response.ts',
-    'src/Router.ts',
-    'src/middlewares/parse-cookies.ts',
-    'src/middlewares/parse-body-to-json.ts',
-  ],
+  entry: ['index.ts', 'src/**/*.ts'],
   splitting: false,
   sourcemap: true,
   clean: true,
