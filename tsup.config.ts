8aac786bec41a6389a7863ee092e582fca6b86f6
janossik <marcinczaniecki97@gmail.com>
2023-07-16 01:51:58 +0200

    fixed tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['index.ts', 'src/**/*.ts'],
-  splitting: false,
-  sourcemap: true,
+  splitting: true,
+  sourcemap: false,
   clean: true,
   outDir: 'lib',
 });
