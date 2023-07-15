0c884bf6a61788d729068486c3950485854eeb53
janossik <marcinczaniecki97@gmail.com>
2023-07-16 01:12:45 +0200

    added params for Request

--- src/Request.ts
+++ src/Request.ts
@@ -3,10 +3,21 @@ import { Socket } from 'net';
 
 export class Request<TBody = Record<string, unknown>> extends IncomingMessage {
   body: TBody = {} as TBody;
+  private _params: Record<string, string> = {};
   method = '';
   private _cookies: Record<string, string> = {};
   constructor(args: Socket) {
     super(args);
+  }
+
+  get params() {
+    return this._params;
+  }
+  set params(value: Record<string, string>) {
+    this._params = value;
+  }
+  set param({ name, value }: { name: string; value: string }) {
+    this._params[name] = value;
   }
 
   set cookies(value: Record<string, string>) {
