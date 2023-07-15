e88ed0b08c34511e9dff59a8f41743fa61033e54
janossik <marcinczaniecki97@gmail.com>
2023-07-16 01:16:12 +0200

    For default export exmano added to use two middlewares: parseBodyToJson, parseCookies

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { Appltication } from './src/Application';
 import { Server } from './src/Server';
+import { parseBodyToJson } from './src/middlewares/parse-body-to-json';
+import { parseCookies } from './src/middlewares/parse-cookies';
 
+export * from './src/utils/router-factory-for-request';
 export * from './src/Server';
 export * from './src/Application';
 export * from './src/Request';
@@ -10,5 +13,8 @@ export * from './src/middlewares/parse-cookies';
 export * from './src/middlewares/parse-body-to-json';
 
 export default function exmano(server?: Server | null) {
-  return new Appltication(server);
+  const app = new Appltication(server);
+  app.use(parseBodyToJson);
+  app.use(parseCookies);
+  return app;
 }
