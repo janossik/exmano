1f90b54d56e63c6967c8a4c7b61a64c2ee7a5d27
janossik <marcinczaniecki97@gmail.com>
2023-08-06 00:38:13 +0200

    Changed test example

--- examples/index.ts
+++ examples/index.ts
@@ -2,23 +2,29 @@ import exmano, { Router } from 'exmano';
 
 const app = exmano();
 
-const router = new Router('/1');
+const router1 = new Router('/1');
 const router2 = new Router('/3');
 const router3 = new Router('/3');
 
-router2.post('/4', (request, response) => {});
-router3.post('/', (request, response) => {});
-router.use('/2/', router2);
-router.use('/2', router3);
+router2.post('/4', (request, response) => {
+  response.send('router2 POST /1/2/3/4');
+});
+router3.post('/', (request, response) => {
+  response.send('router3 POST /1/2/3');
+});
 
-app.get('/1/2/3/4', (request, response) => {});
-app.use(router);
+router1.use('/2', router2);
+router1.use('/2', router3);
+
+app.get('/1/2/3/4', (request, response) => {
+  response.send('router1 GET /1/2/3/4');
+});
+
+app.use(router1);
 
 app.get('/', (request, response) => {
   response.send('Hello World');
 });
-
-console.log(app.routers);
 
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:3000`);
