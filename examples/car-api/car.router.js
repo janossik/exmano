984aa1ac6eb11ae6a693aaa82ca7d273295735e2
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2024-03-12 19:15:29 +0100

    Router example

---
+++ examples/car-api/car.router.js
@@ -0,0 +1,36 @@
+const { Router, HttpError } = require('exmano');
+const fs = require('node:fs');
+
+const carRouter = new Router("cars"); // You can add "cars" to the path using "use" from the app
+
+const getCars = async (name) => {
+    const cars = JSON.parse(await fs.promises.readFile('cars.json', { encoding: "utf-8" }));
+
+    if (typeof name === "string" && Array.isArray(cars)) {
+        return cars.find(car => {
+            if ("name" in car && car.name.toLocaleLowerCase().includes(name.toLocaleLowerCase())) {
+                return car;
+            }
+        })
+    }
+
+    return cars;
+};
+
+
+carRouter.get("/", async (req, res) => {
+    const cars = await getCars();
+    res.json(cars);
+});
+
+carRouter.get("/:name", async (req, res) => {
+    const car = await getCars(req.params.name);
+    if (!car) {
+        throw new HttpError("Not found", 404)
+    }
+    res.json(car);
+});
+
+
+
+module.exports = carRouter;
---
+++ examples/car-api/index.js
@@ -0,0 +1,15 @@
+const exmano = require('exmano');
+const carRouter = require('./car.router');
+
+const app = exmano();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Wlecome in car API');
+});
+
+app.use(carRouter);
+
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost${port}`);
+});
